14851ff1a33381ae6184d24b6aa60cfff13e542d
jiangmengxia <jiangmengxia@qq.com>
2024-09-17 23:22:39 +0800

    redux demos

--- redux/demos/middlewares/logger.js
+++ redux/demos/middlewares/logger.js
@@ -2,14 +2,14 @@
  * @Author: jiangmengxia jiangmengxia@qq.com
  * @Date: 2024-09-17 13:09:51
  * @LastEditors: jiangmengxia jiangmengxia@qq.com
- * @LastEditTime: 2024-09-17 13:12:48
- * @FilePath: /jiangmengxia.github.io/redux/demos/middlewares/looger.js
+ * @LastEditTime: 2024-09-17 20:44:41
+ * @FilePath: /jiangmengxia.github.io/redux/demos/middlewares/logger.js
  * @Description: Description
  */
 export default (store) => (next) => (action) => {
   console.log("store", store);
   console.log("action", action);
-  let result = next(action); // next(action)执行完之后，返回的结果
+  let result = next(action); // next(action)执行完之后，返回的结果, next方法实际就是dispatch方法
   console.log("next Store", store.getState()); // 打印执行完action后的store
   return result;
 };
---
+++ redux/demos/redux/bindActionCreators/index.js
@@ -0,0 +1,161 @@
+/*
+ * @Author: jiangmengxia jiangmengxia@qq.com
+ * @Date: 2024-09-17 16:02:16
+ * @LastEditors: jiangmengxia jiangmengxia@qq.com
+ * @LastEditTime: 2024-09-17 22:50:56
+ * @FilePath: /jiangmengxia.github.io/redux/demos/redux/bindActionCreators/index.js
+ * @Description: Description
+ */
+
+// 实现与thunk方法一样的功能，：dispatch允许分发异步action
+function enhancer(createStore) {
+  return function (reducer, preloadState) {
+    // 创建store
+    const store = createStore(reducer, preloadState);
+    const dispatch = store.dispatch;
+
+    // 增强的dispatch方法,做的事跟实现的thunk方法一样
+    function _dispatch(action) {
+      // 异步action
+      if (typeof action === "function") {
+        return action(dispatch);
+      }
+      // 同步的，调用原本的dispatch方法
+      dispatch(action);
+    }
+
+    // 返回store
+    return {
+      ...store,
+      dispatch: _dispatch, // 重写dispatch方法
+    };
+  };
+}
+
+function createStore(reducer, preloadState, enhancer) {
+  // 约束reducer函数类型
+  if (typeof reducer !== "function") {
+    throw new Error("reducer必须是一个函数");
+  }
+  if (typeof enhancer !== "undefined") {
+    if (typeof enhancer !== "function") {
+      throw new Error("enhancer必须是一个函数");
+    }
+    // 自己去创建自定义的store，实现更强大的store功能
+    return enhancer(createStore)(reducer, preloadState);
+  }
+
+  // 表示store对象中存储的全局状态
+  let state = preloadState;
+  // 存储订阅者函数集合
+  let listeners = [];
+
+  // 获取状态的
+  function getState() {
+    return state;
+  }
+
+  // 方法用来触发action
+  function dispatch(action) {
+    // 判断action是否是对象
+    if (!isPlainObject(action)) {
+      throw new Error("action必须是一个对象");
+    }
+    if (action.type === undefined) {
+      throw new Error("action必须有一个type属性");
+    }
+    // 调用reducer函数，更新全局状态
+    state = reducer(state, action);
+    // 触发订阅者函数
+    listeners.forEach((listener) => listener());
+  }
+
+  // 订阅方法
+  function subscribe(listener) {
+    listeners.push(listener);
+    // 返回一个函数，用来取消订阅
+    return function () {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  }
+  return {
+    getState,
+    dispatch,
+    subscribe,
+  };
+}
+function isPlainObject(obj) {
+  // 排除基本数据类型和空值
+  if (typeof obj !== "object" || obj === null) return false;
+  //区分数组和对象
+  //   if (Object.getPrototypeOf(obj) === Object.prototype) return true;
+  //   return false;
+  let proto = obj;
+  while (Object.getPrototypeOf(proto) !== null) {
+    proto = Object.getPrototypeOf(proto);
+  }
+  return Object.getPrototypeOf(obj) === proto;
+}
+
+// 1. 创建一个 reducer
+function counter(state = 0, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1,
+      };
+    case "DECREMENT":
+      return {
+        count: state.count - 1,
+      };
+    default:
+      return state;
+  }
+}
+
+//
+function doCounter() {
+  // 2. 创建一个 store
+  const store = createStore(counter, { count: 0 });
+
+  // 3. 订阅 store 的变化
+  store.subscribe(() => {
+    console.log(store.getState());
+    document.getElementById("counter").innerText = store.getState().count;
+  });
+
+  //
+  function bindActionCreators(actionCreators, dispatch) {
+    let bindActionCreators = {};
+    Object.keys(actionCreators).forEach((key) => {
+      bindActionCreators[key] = function () {
+        const action = actionCreators[key]();
+        console.log("action", action);
+        dispatch(action);
+      };
+    });
+    return bindActionCreators;
+  }
+
+  // actionCreator函数
+  function increment() {
+    return { type: "INCREMENT" };
+  }
+  // actionCreator函数
+  function decrement() {
+    return { type: "DECREMENT" };
+  }
+
+  // actions包含两个action方法，increment和decrement，可以直接调用来触发对应的action执行
+  const actions = bindActionCreators({ increment, decrement }, store.dispatch);
+  console.log("actions", actions);
+
+  document.getElementById("btnInc").onclick = () => {
+    actions.increment();
+  };
+  document.getElementById("btnDec").onclick = () => {
+    actions.decrement();
+  };
+}
+
+doCounter();
